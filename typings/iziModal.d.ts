ed62eb47b29bd236ea0c865771eb6f75fe7e1ca1
voltsonic <6464019+voltsonic@users.noreply.github.com>
2019-12-15 14:26:17 -0700

    iziModal generic typings

---
+++ typings/iziModal.d.ts
@@ -0,0 +1,75 @@
+// Very basic typings for iziModal until typings are generated.
+// Designed for v1.6.0
+
+// Note: No need to import to separate project unless you need typings for raw access to $.iziModal()
+
+// Jquery Wrap
+interface JQuery<HTMLElement> {
+    iziModal(): void;
+    iziModal(options: any): void;
+    iziModal(methodKey: string, options?: any): void;
+}
+
+// Transitions Options
+interface IziModalTransitions {
+    transitionIn?: string,
+    transitionOut?: string,
+}
+
+// Basic iziModal Settings Block
+interface IziModalSettings {
+    title?: string,
+    subtitle?: string,
+    headerColor?: string,
+    background?: null|string,
+    theme?: string,
+    icon?: null|string,
+    iconText?: null|string,
+    iconColor?: string,
+    rtl?: boolean,
+    width?: number|string,
+    height?: number|string,
+    top?: null|number|string,
+    bottom?: null|number|string,
+    borderBottom?: boolean,
+    padding?: number,
+    radius?: number,
+    zindex?: number,
+    iframe?: boolean,
+    iframeHeight?: number,
+    iframeURL?: null|string,
+    focusInput?: boolean,
+    group?: string,
+    loop?: boolean,
+    arrowKeys?: boolean,
+    navigateCaption?: boolean,
+    navigateArrows?: boolean | 'closeToModal' | 'closeScreenEdge',
+    history?: boolean,
+    restoreDefaultContent?: boolean,
+    autoOpen?: boolean|number,
+    bodyOverflow?: boolean,
+    fullscreen?: boolean,
+    openFullscreen?: boolean,
+    closeOnEscape?: boolean,
+    closeButton?: boolean,
+    appendTo?: boolean|string,
+    appendToOverlay?: false|string,
+    overlay?: boolean,
+    overlayClose?: boolean,
+    overlayColor?: string,
+    timeout?: false|number,
+    timeoutProgressbar?: boolean,
+    pauseOnHover?: boolean,
+    timeoutProgressbarColor?: string,
+    transitionIn?: string,
+    transitionOut?: string,
+    transitionInOverlay?: string,
+    transitionOutOverlay?: string,
+    onFullscreen?: () => void,
+    onResize?: () => void,
+    onOpening?: () => void,
+    onOpened?: () => void,
+    onClosing?: () => void,
+    onClosed?: () => void,
+    afterRender?: () => void
+}
