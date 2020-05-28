8204e4b155ba3096be0471274c32b2edf2042911
voltsonic <6464019+voltsonic@users.noreply.github.com>
2020-05-27 20:50:44 -0600

    removed old typing format for legacy style

--- src/Types/Themes/ThemesTypes.ts
+++
@@ -1,4 +0,0 @@
-export namespace ThemeTypes {
-    export type Core = "add" | "edit" | "delete" | "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark";
-    export type All = Core | string;
-}
--- src/Types/iziModalWrapTypes.ts
+++
@@ -1,5 +0,0 @@
-import {ThemeTypes} from "./Themes/ThemesTypes";
-
-export namespace iziModalWrapTypes {
-    export import Themes = ThemeTypes;
-}
