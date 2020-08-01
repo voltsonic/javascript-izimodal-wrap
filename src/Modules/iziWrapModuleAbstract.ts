197c57f0f1262b6e2464f85369717b221c037862
voltsonic <6464019+voltsonic@users.noreply.github.com>
2020-08-01 17:23:39 -0600

    created an abstract module class thats extended from

---
+++ src/Modules/iziWrapModuleAbstract.ts
@@ -0,0 +1,13 @@
+"use strict";
+
+import iziModalWrap from "../iziModalWrap";
+
+export default abstract class iziWrapModuleAbstract {
+    protected w: iziModalWrap;
+    constructor(modal: iziModalWrap) {
+        this.w = modal;
+        this.init();
+    }
+
+    init(){}
+}
