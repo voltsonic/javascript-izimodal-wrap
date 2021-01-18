88daa412e74a9331cf1f7796b5af8eb8207854be
voltsonic <6464019+voltsonic@users.noreply.github.com>
2021-01-17 23:57:29 -0700

    fixed bad url
    not sure how bad url occurred, check this later on to make sure it is still correct. (for ref in future if needed, on stackoverflow under a question titled "How to deep merge instead of shallow merge?"

--- src/Utils/MergeDeep.ts
+++ src/Utils/MergeDeep.ts
@@ -1,8 +1,8 @@
 "use strict";
 
 /**
- * Copied from https://stackoverflow.com/a/15866781
- * Refactored on 2020-08-01 by voltsonic.
+ * Copied from https://stackoverflow.com/a/34749873
+ * Refactored on 2020-08-01, 2021-01-12 by voltsonic.
  */
 export default class MergeDeep {
     static isObject(item: any){
@@ -10,7 +10,7 @@ export default class MergeDeep {
     }
 
     static combine(target: any, ...sources: any): any {
-        if (!sources.length) return target;
+        if (!sources || !sources.length) return target;
         const source = sources.shift();
 
         if (this.isObject(target) && this.isObject(source)) {
@@ -18,7 +18,7 @@ export default class MergeDeep {
                 if(source.hasOwnProperty(key)){
                     if (this.isObject(source[key])) {
                         if (!target[key]) Object.assign(target, { [key]: {} });
-                        this.combine(target[key], source[key]);
+                        target[key] = this.combine(target[key], source[key]);
                     } else {
                         Object.assign(target, { [key]: source[key] });
                     }
@@ -26,6 +26,6 @@ export default class MergeDeep {
             }
         }
 
-        return this.combine(target, ...sources);
+        return this.combine(target, ...(sources ?? []));
     }
 }
