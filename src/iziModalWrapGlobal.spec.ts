7707d795694c3f34128adef3585eb28534bb02a7
voltsonic <6464019+voltsonic@users.noreply.github.com>
2021-01-18 00:00:04 -0700

    updated global to a better integration with themes
    - Allows more global options as well so your not having to have duplicate code if all your "add" panels have the title "Adding"
    
    - Included a .spec. test for basic tests

---
+++ src/iziModalWrapGlobal.spec.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+import chai from 'chai';
+import 'mocha';
+import iziModalWrapGlobal from "./iziModalWrapGlobal";
+chai.use(require('chai-fs'));
+require('jsdom-global')();
+
+describe('iziModalWrapGlobalInner // Basics', () => {
+    it(`default config check.`, () => {
+        chai.assert.equal(
+            JSON.stringify(iziModalWrapGlobal.getSettings()),
+            '{"classes":{"modals":{"open":"{prefixId}-open"},"modal":{"open":"{prefixId}-open-{modalId}","opened":"{prefixId}-opened-{modalId}","setup":"{prefixId}-ran-{modalId}"}},"statics":{"prefixId":"izimw","isMobileDevice":false,"layerUpBase":1072},"themes":{"add":{"color":"#2C5937","icon":""},"edit":{"color":"#2364AA","icon":""},"delete":{"color":"#930119","icon":""},"primary":{"color":"#007bff","icon":""},"secondary":{"color":"#6c757d","icon":""},"success":{"color":"#28a745","icon":""},"danger":{"color":"#dc3545","icon":""},"warning":{"color":"#ffc107","icon":""},"info":{"color":"#17a2b8","icon":""},"light":{"color":"#f8f9fa","icon":""},"dark":{"color":"#343a40","icon":""}},"widths":{"a_xs":350,"b_sm":467,"c_md":603,"d_lg":730,"e_xl":882}}',
+            'Default settings no longer match.',
+        );
+    });
+    it(`modified theme values.`, () => {
+        iziModalWrapGlobal
+            .themeAdd('theme_key', '#000000')
+            .themeAdd('theme_key2', '#FFFFFF', 'ico')
+        ;
+        const s = iziModalWrapGlobal.getSettings();
+        chai.assert.isDefined(s.themes['theme_key'], 'Missing theme_key.');
+        chai.assert.isDefined(s.themes['theme_key'].color, 'Missing color theme_key.');
+        chai.assert.isUndefined(s.themes['theme_key'].icon, 'Has icon theme_key?!?!');
+        chai.assert.isDefined(s.themes['theme_key2'], 'Missing theme_key2.');
+        chai.assert.isDefined(s.themes['theme_key2'].color, 'Missing color theme_key2.');
+        chai.assert.isDefined(s.themes['theme_key2'].icon, 'Missing icon theme_key2.');
+    });
+});
--- src/iziModalWrapGlobal.ts
+++ src/iziModalWrapGlobal.ts
@@ -1,7 +1,19 @@
-import deepmerge from "deepmerge";
+"use strict";
+
+import MergeDeep from "./Utils/MergeDeep";
 
 export type TThemeTypesBuiltIn = "add" | "edit" | "delete" | "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark";
 export type TThemeTypesAll = TThemeTypesBuiltIn | string;
+
+type IIMWGlobalIconType = string | (() => (string | undefined));
+type IIMWGlobalTitlesType = string | (() => (string | undefined));
+
+interface IIMWGlobalTheme {
+    color?: string,
+    icon?: IIMWGlobalIconType,
+    title?: IIMWGlobalTitlesType,
+    subtitle?: IIMWGlobalTitlesType,
+}
 
 export interface IIMWGlobalSettings {
     classes?: {
@@ -17,47 +29,29 @@ export interface IIMWGlobalSettings {
     statics?: {
         layerUpBase?: number,
         prefixId?: string,
-        mobileMode?: boolean
+        mobileMode?: boolean,
+        isMobileDevice?: boolean | (() => boolean)
     },
-    theme?: {
-        colors?: {
-            add?        : string,
-            edit?       : string,
-            delete?     : string,
-            primary?    : string,
-            secondary?  : string,
-            success?    : string,
-            danger?     : string,
-            warning?    : string,
-            info?       : string,
-            light?      : string,
-            dark?       : string,
-            [extras: string]: string,
-        },
-        icons?: {
-            add?        : string,
-            edit?       : string,
-            delete?     : string,
-            primary?    : string,
-            secondary?  : string,
-            success?    : string,
-            danger?     : string,
-            warning?    : string,
-            info?       : string,
-            light?      : string,
-            dark?       : string,
-            [extras: string]: string,
-        },
-        widths?: {
-            a_xs?: number,
-            b_sm?: number,
-            c_md?: number,
-            d_lg?: number,
-            e_xl?: number,
-        }
+    widths?: {
+        a_xs?: number,
+        b_sm?: number,
+        c_md?: number,
+        d_lg?: number,
+        e_xl?: number,
+    }
+    themes?: {
+        [themeKey in TThemeTypesAll]: IIMWGlobalTheme
     },
 }
-interface IIMWGlobalSettingsInner {
+
+interface IIMWGlobalThemeStatic {
+    color: string,
+    icon?: IIMWGlobalIconType,
+    title?: IIMWGlobalTitlesType,
+    subtitle?: IIMWGlobalTitlesType,
+}
+
+interface IIMWGlobalSettingsStatic {
     classes: {
         modals: {
             open: string
@@ -71,110 +65,103 @@ interface IIMWGlobalSettingsInner {
     statics: {
         layerUpBase: number,
         prefixId: string,
+        mobileMode: boolean,
         isMobileDevice: boolean | (() => boolean)
     },
-    theme: {
-        colors: {
-            add        : string,
-            edit       : string,
-            delete     : string,
-            primary    : string,
-            secondary  : string,
-            success    : string,
-            danger     : string,
-            warning    : string,
-            info       : string,
-            light      : string,
-            dark       : string,
-            [extras: string]: string,
-        },
-        icons: {
-            add?        : string,
-            edit?       : string,
-            delete?     : string,
-            primary?    : string,
-            secondary?  : string,
-            success?    : string,
-            danger?     : string,
-            warning?    : string,
-            info?       : string,
-            light?      : string,
-            dark?       : string,
-            [extras: string]: undefined | string,
-        },
-        widths: {
-            a_xs: number,
-            b_sm: number,
-            c_md: number,
-            d_lg: number,
-            e_xl: number,
-        }
+    widths: {
+        a_xs: number,
+        b_sm: number,
+        c_md: number,
+        d_lg: number,
+        e_xl: number,
+    }
+    themes: {
+        [themeKey in TThemeTypesAll]: IIMWGlobalThemeStatic
     },
 }
 
-export default class iziModalWrapGlobal {
-    protected static globalSettings: null | IIMWGlobalSettingsInner = null;
-    private static __rootStyle: false | CSSStyleDeclaration = false;
+class iziModalWrapGlobalInner {
+    protected __init = false;
+    protected globalSettings: IIMWGlobalSettingsStatic;
+    protected __rootStyle: false | CSSStyleDeclaration = false;
 
-    private static __getRootStyle(): CSSStyleDeclaration {
+    protected __getRootStyle(): CSSStyleDeclaration {
         if(!this.__rootStyle){
             this.__rootStyle = getComputedStyle(document.querySelector('body'));
         }
         return this.__rootStyle;
     }
-    private static getPropVal(key: string, _default: string = '#000'): string {
+    protected getPropVal(key: string, _default: string = '#000'): string {
         const r = this.__getRootStyle().getPropertyValue("--"+key);
         if(r.length > 0)
             return r;
         return _default;
     }
 
-    public static init(merge: IIMWGlobalSettings = {}){
-        if(this.globalSettings)
-            throw new Error("Not allowed to trigger iziModalWrapGlobal.init() twice. These settings are meant to be shared between modal instances.");
+    public themeGet(key: TThemeTypesAll): undefined | IIMWGlobalThemeStatic {
+        this.init();
+        if(this.globalSettings.themes.hasOwnProperty(key))
+            return this.globalSettings.themes[key];
+        return undefined;
+    }
 
-        const baseSettings: IIMWGlobalSettingsInner = {
-            classes: {
-                modals: {
-                    open: "{prefixId}-open"
+    public themeAdd(
+        key: string,
+        color: string,
+        icon?: IIMWGlobalIconType,
+        title?: IIMWGlobalTitlesType,
+        subtitle?: IIMWGlobalTitlesType
+    ): iziModalWrapGlobalInner {
+        this.init();
+        if(this.globalSettings.themes.hasOwnProperty(key))
+            throw new Error(`Attempting to add "${key}" twice for iziModalWrapGlobal.customize.addTheme();`);
+
+        this.globalSettings.themes[key] = {
+            color,
+            icon,
+            title,
+            subtitle,
+        };
+
+        return this;
+    }
+
+    /**
+     *
+     * @param merge
+     * @param forceReInit used in testing.
+     */
+    public init(merge: IIMWGlobalSettings = {}, forceReInit: boolean = false): iziModalWrapGlobalInner {
+        if(!this.__init || forceReInit){
+            this.__init = true;
+            this.globalSettings = MergeDeep.combine(({
+                classes: {
+                    modals: {
+                        open: "{prefixId}-open"
+                    },
+                    modal: {
+                        open: "{prefixId}-open-{modalId}",
+                        opened: "{prefixId}-opened-{modalId}",
+                        setup: "{prefixId}-ran-{modalId}"
+                    }
                 },
-                modal: {
-                    open: "{prefixId}-open-{modalId}",
-                    opened: "{prefixId}-opened-{modalId}",
-                    setup: "{prefixId}-ran-{modalId}"
-                }
-            },
-            statics: {
-                prefixId: "izimw",
-                isMobileDevice: false,
-                layerUpBase: 1072,
-            },
-            theme: {
-                colors: {
-                    add         : "#2C5937",
-                    edit        : "#2364AA",
-                    delete      : "#930119",
-                    primary     : this.getPropVal("--primary",  "#007bff"),
-                    secondary   : this.getPropVal("--secondary","#6c757d"),
-                    success     : this.getPropVal("--success",  "#28a745"),
-                    danger      : this.getPropVal("--danger",   "#dc3545"),
-                    warning     : this.getPropVal("--warning",  "#ffc107"),
-                    info        : this.getPropVal("--info",     "#17a2b8"),
-                    light       : this.getPropVal("--light",    "#f8f9fa"),
-                    dark        : this.getPropVal("--dark",     "#343a40"),
+                statics: {
+                    prefixId: "izimw",
+                    isMobileDevice: false,
+                    layerUpBase: 1072,
                 },
-                icons: {
-                    add         : "",
-                    edit        : "",
-                    delete      : "",
-                    primary     : "",
-                    secondary   : "",
-                    success     : "",
-                    danger      : "",
-                    warning     : "",
-                    info        : "",
-                    light       : "",
-                    dark        : "",
+                themes: {
+                    add: {          color: this.getPropVal("--add", "#2C5937"),         icon: this.getPropVal("--add",  "") },
+                    edit: {         color: this.getPropVal("--edit", "#2364AA"),        icon: this.getPropVal("--edit",  "") },
+                    delete: {       color: this.getPropVal("--delete", "#930119"),      icon: this.getPropVal("--delete",  "") },
+                    primary: {      color: this.getPropVal("--primary",  "#007bff"),    icon: this.getPropVal("--primary",  "") },
+                    secondary: {    color: this.getPropVal("--secondary","#6c757d"),    icon: this.getPropVal("--secondary",  "") },
+                    success: {      color: this.getPropVal("--success",  "#28a745"),    icon: this.getPropVal("--success",  "") },
+                    danger: {       color: this.getPropVal("--danger",   "#dc3545"),    icon: this.getPropVal("--danger",  "") },
+                    warning: {      color: this.getPropVal("--warning",  "#ffc107"),    icon: this.getPropVal("--warning",  "") },
+                    info: {         color: this.getPropVal("--info",     "#17a2b8"),    icon: this.getPropVal("--info",  "") },
+                    light: {        color: this.getPropVal("--light",    "#f8f9fa"),    icon: this.getPropVal("--light",  "") },
+                    dark: {         color: this.getPropVal("--dark",     "#343a40"),    icon: this.getPropVal("--dark",  "") },
                 },
                 widths: {
                     a_xs: 350,
@@ -182,27 +169,17 @@ export default class iziModalWrapGlobal {
                     c_md: 603,
                     d_lg: 730,
                     e_xl: 882,
-                }
-            },
-        };
-        // @ts-ignore.
-        this.globalSettings = deepmerge(baseSettings, merge);
-
-        const meRoot = this;
-        return {
-            addTheme: function(key: string, color: string, icon?: string){
-                if(meRoot.globalSettings.theme.colors.hasOwnProperty(key)){
-                    throw new Error(`Attempting to add "${key}" twice for iziModalWrapGlobal.customize.addTheme();`)
-                }
-                meRoot.globalSettings.theme.colors[key] = color;
-                meRoot.globalSettings.theme.icons[key] = icon;
-                return this;
-            },
-            end: (): iziModalWrapGlobal => meRoot,
-        };
+                },
+            } as IIMWGlobalSettings), merge);
+        }
+        return this;
     }
 
-    public static getSettings(): IIMWGlobalSettingsInner {
+    public getSettings(): IIMWGlobalSettings {
+        this.init();
         return this.globalSettings;
     }
 }
+
+const iziModalWrapGlobal = new iziModalWrapGlobalInner();
+export default iziModalWrapGlobal;
