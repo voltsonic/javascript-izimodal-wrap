93510bff69a20491c851cb39a3e7a6cbbca278fc
voltsonic <6464019+voltsonic@users.noreply.github.com>
2021-01-17 23:55:54 -0700

    removed old style

--- src/@types-internal/iziModalWrapThemeWrap.d.ts
+++
@@ -1,6 +0,0 @@
-interface iziModalWrapThemeWrap {
-    it: (themeKey: string) => iziModalWrapThemeWrap;
-    title: (title: string, subTitle?: string) => iziModalWrapThemeWrap;
-    icon: (icon: string, iconText?: string) => iziModalWrapThemeWrap;
-    end: () => iziModalWrap;
-}
