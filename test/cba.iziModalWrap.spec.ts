ebdbcef78998212a41699166aaea284e31f42ab7
voltsonic <6464019+voltsonic@users.noreply.github.com>
2021-01-30 12:00:32 -0700

    test(izimodalwrap): fixed and setup basic tests for iziModalWrap

--- test/cba.iziModalWrap.spec.ts
+++ test/cba.iziModalWrap.spec.ts
@@ -1,28 +1,56 @@
-// // @todo figure why jQuery won't inject into iziModalWrap via jsdom-global, it works inside describe/it but not when the inner class gets setup.
-// 'use strict';
-//
-// import chai from 'chai';
-// import 'mocha';
-// import iziModalWrap from '../src/iziModalWrap';
-// // chai.use(require('chai-fs'));
-//
-// // Dom + jQuery
-// // const jQuery = require('jquery');
-// // require('jsdom-global')();
-// // const $ = jQuery(window);
-// // @ts-ignore.
-// // global.$ = $; // make availble to other files if necessary
-//
-// describe('iziModalWrap // Basics', () => {
-//     // $('body').html('<div id="izimw-test" class="iziModal"></div>');
-//     // let modal: iziModalWrap;
-//     it('Setup Modal', () => {
-//         // modal = new iziModalWrap('test');
-//         // console.log(document.body.innerHTML);
-//         // jsdom.jQueryify(window, "../node_modules/jquery/dist/jquery.min.js", () => {
-//         //     // modal = new iziModalWrap('test');
-//         // });
-//         chai.assert.equal(iziModalWrap.VERSION, '1.0.0', 'Version match fail');
-//         // chai.assert.fail('no.test');
-//     });
-// });
+'use strict';
+
+import chai from 'chai';
+import 'mocha';
+import iziModalWrap from '../src/iziModalWrap';
+
+// tslint:disable-next-line:no-var-requires
+const _package = require('../package.json');
+
+describe('iziModalWrap // Basics', () => {
+    it('Package Version', () =>
+        chai.assert.equal(iziModalWrap.VERSION, _package.version, 'Version match fail'));
+    it('Setup Modal', () => {
+        // @ts-ignore
+        const m = (new iziModalWrap('test'));
+        chai.assert.isNotNull(document.querySelector('#izimw-test'), 'Modal not created?');
+    });
+    it('Events via Config Options.', () => {
+        // @ts-ignore
+        const m = (new iziModalWrap({
+            modalId: 'test2',
+            events: {
+                opening: () => {
+                    // tslint:disable-next-line:no-console
+                    console.log('opening');
+                },
+                opened: () => {
+                    // tslint:disable-next-line:no-console
+                    console.log('opened');
+                },
+                after_render: () => {
+                    // tslint:disable-next-line:no-console
+                    console.log('after_render');
+                },
+                fullscreen: () => {
+                    // tslint:disable-next-line:no-console
+                    console.log('fullscreen');
+                },
+                resize: () => {
+                    // tslint:disable-next-line:no-console
+                    console.log('resize');
+                },
+                closing: () => {
+                    // tslint:disable-next-line:no-console
+                    console.log('closing');
+                },
+                closed: () => {
+                    // tslint:disable-next-line:no-console
+                    console.log('closed');
+                }
+            },
+        }));
+        chai.assert.isNotNull(document.querySelector('#izimw-test'), 'Modal not created?');
+        m.methods.display.open();
+    });
+});
